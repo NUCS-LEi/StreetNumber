f246c9c6b877090aefe1fec8a5bfc663158923c0
Wang <lwang@litle.com>
2017-10-02 10:59:25 -0400

    update

--- freeze.py
+++ freeze.py
@@ -1,12 +1,13 @@
+import tensorflow as tf
 from tensorflow.python.tools import freeze_graph
 from tensorflow.python.tools import optimize_for_inference_lib
 
 with tf.Session() as sess:
-    saver = tf.train.import_meta_graph('logs\trains\model.ckpt.meta')
-    saver.restore(sess, "logs\trains\model.ckpt")
+    saver = tf.train.import_meta_graph('logs\\trains2\\model.ckpt-352000.meta')
+    saver.restore(sess, "logs\\trains2\\model.ckpt-352000")
 
-freeze_graph.freeze_graph(input_graph = "logs\trains\model.pbtxt",  input_saver = "",
-             input_binary = False, input_checkpoint = "logs\trains\model.ckpt", output_node_names = "y_",
+freeze_graph.freeze_graph(input_graph = "logs\\trains2\\model.pbtxt",  input_saver = "",
+             input_binary = False, input_checkpoint = "logs\\trains2\\model.ckpt-352000", output_node_names = "y_",
              restore_op_name = "save/restore_all", filename_tensor_name = "save/Const:0",
              output_graph = "streetnumber.pb", clear_devices = True, initializer_nodes = "")
 
