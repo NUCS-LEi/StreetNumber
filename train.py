9eeaf90be15e880d10eda20bcb79fac28b2590f3
nucs.lei <nucs.lei@gmail.com>
2019-02-12 22:05:25 -0800

    update

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ tf.app.flags.DEFINE_string('restore_checkpoint', None,
                            'Path to restore checkpoint (without postfix), e.g. .\\logs\\train\\model.ckpt-100')
 tf.app.flags.DEFINE_integer('batch_size', 32, 'Default 32')
 tf.app.flags.DEFINE_float('learning_rate', 1e-2, 'Default 1e-2')
-tf.app.flags.DEFINE_integer('patience', 40, 'Default 100, set -1 to train infinitely')
+tf.app.flags.DEFINE_integer('patience', 100, 'Default 100, set -1 to train infinitely')
 tf.app.flags.DEFINE_integer('decay_steps', 10000, 'Default 10000')
 tf.app.flags.DEFINE_float('decay_rate', 0.9, 'Default 0.9')
 FLAGS = tf.app.flags.FLAGS
@@ -90,8 +90,7 @@ def _train(path_to_train_tfrecords_file, num_train_examples, path_to_val_tfrecor
 
                 if accuracy > best_accuracy:
                     tf.train.write_graph(sess.graph_def, '.', os.path.join(path_to_train_log_dir, 'model.pbtxt'))
-                    path_to_checkpoint_file = saver.save(sess, os.path.join(path_to_train_log_dir, 'model.ckpt'),
-                                                         global_step=global_step_val)
+                    path_to_checkpoint_file = saver.save(sess, os.path.join(path_to_train_log_dir, 'model.ckpt'))
                     print('=> Model saved to file: %s' % path_to_checkpoint_file)
                     patience = initial_patience
                     best_accuracy = accuracy
